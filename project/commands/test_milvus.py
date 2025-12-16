6f5bbeb08b7e51f07ff73dcd03475ffa67b9ce5b
SodaShikenn <zixuanhe43@gmail.com>
2025-12-16 19:07:52 +0900

    CRUD in Milvus DB

--- project/commands/test_milvus.py
+++ project/commands/test_milvus.py
@@ -5,8 +5,43 @@ from apps.dataset.milvus_models import DatasetMilvusModel
 
 @click.command("test_milvus")
 def run():
-    # Check Connection Status
+    # Check connection status
     if connections.has_connection("default"):
         print("Successfully connected to Milvus")
     else:
         print("Failed to connect to Milvus")
+
+    # # Initialize the collection and create an index
+    # DatasetMilvusModel.init_collection()
+    # DatasetMilvusModel.create_index()
+
+    # # Insert data
+    # data = [
+    #     {"dataset_id": 1, "document_id": 1, "segment_id": 1, "text_vector": [random() for i in range(1536)]},
+    #     {"dataset_id": 1, "document_id": 1, "segment_id": 2, "text_vector": [random() for i in range(1536)]},
+    #     {"dataset_id": 2, "document_id": 2, "segment_id": 1, "text_vector": [random() for i in range(1536)]},
+    #     {"dataset_id": 2, "document_id": 2, "segment_id": 2, "text_vector": [random() for i in range(1536)]},
+    #     {"dataset_id": 2, "document_id": 2, "segment_id": 3, "text_vector": [random() for i in range(1536)]},
+    # ]
+    # DatasetMilvusModel.insert(data)
+
+    # # Query data
+    # records = DatasetMilvusModel.query('dataset_id==2')
+    # print(records)
+    # print(len(records))
+
+    # # View data amount
+    # print(DatasetMilvusModel.get_entity_count())
+
+    # # Delete data
+    # delete_expr = 'dataset_id==2'
+    # DatasetMilvusModel.delete(delete_expr)
+
+    # # Delete collection
+    # DatasetMilvusModel.drop_collection()
+
+    # 向量检索
+    query_vector = [random() for i in range(1536)]
+    records = DatasetMilvusModel.search([query_vector], 3)
+    for record in records[0]:
+        print(record)
